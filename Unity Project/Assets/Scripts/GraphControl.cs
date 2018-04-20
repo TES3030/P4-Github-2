ad3889df208d94fe8c30b70a3527b1f59672ba4b
Tobias <tobse.tes@gmail.com>
2018-04-20 15:40:11 +0200

    lots of nice things
    added initial graph functionality

--- Unity Project/Assets/Scripts/GraphControl.cs
+++ Unity Project/Assets/Scripts/GraphControl.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class GraphControl : MonoBehaviour {
     
     public float amplitudeFactor; //This var is not used atm
-    public int xLength; // Gameobjects to create in Unity
+    [Range(10,100)] //this makes the int a slider in the inspector
+    public int xLength = 10; // Gameobjects to create in Unity
 
     public float pointSpacing = 1f; //the space in between each point
         
@@ -15,7 +16,7 @@ public class GraphControl : MonoBehaviour {
     private GameObject wavePrefab; //prefab form which the waveoutline is made
 
     List<GameObject> pointsList = new List<GameObject>(); //the list of all points in a curve
-   
+
     //code not needed at the moment (NEVER DELETE CODE)
     /*
     public void PrintInitialValue() //Run on start to print initial values
@@ -27,7 +28,7 @@ public class GraphControl : MonoBehaviour {
             }
     }*/
 
-   
+
     //Code to randomly change points values - only used for developing
     public void randomChange() //Puts in random values across all Gameobjects
     {
@@ -59,30 +60,40 @@ public class GraphControl : MonoBehaviour {
     {
         GameObject wavePositionParent = new GameObject("WavePosition");//the empty game object containing the position of the curve/wave
         wavePositionParent.transform.rotation = Quaternion.Euler(0,90,0);
+        wavePositionParent.transform.position = new Vector3(0, 0, 0.2f);
         //Vector3 tempPlayerPos = GameObject.Find("Player").transform.localPosition;//the position of the player in order to spawn curve in fornt of player
         //tempPlayerPos.x -= 10; //offsetting curve to be 10 units in x in front
         //wavePositionParent.transform.localPosition = tempPlayerPos; //changing the position of curve/wave
         GameObject waveOutline = (GameObject)Instantiate(wavePrefab, wavePositionParent.transform.localPosition, wavePositionParent.transform.localRotation, wavePositionParent.transform) as GameObject;//instantiating the pink outline arround points
-
-        //Vector3 pointVec = new Vector3(waveOutline.transform.localPosition.x, waveOutline.transform.localPosition.y, waveOutline.transform.localPosition.z- ((pointSpacing + xPoint.transform.localScale.z) * (xLength-1) )/ 2 + pointSpacing);//creating vector of the points created in the for loop below
-        Vector3 pointVec = new Vector3();
+        
+        float step = 2f / xLength *10;
+        Vector3 scale = Vector3.one * step; 
+        Vector3 pointVec;
+        pointVec.z = 0;
         for (int i = 0; i < xLength; i++)//for loop instantiating points and adding them to the list
         {
-            pointVec.x = (i + 0.5f) / 5f - 1f;
+            GameObject point;
+            pointsList.Add(point = (GameObject)Instantiate(xPoint, wavePositionParent.transform.localPosition, Quaternion.Euler(0,90,0)) as GameObject);
+            //adding and instantiating points from the xPoint prefab, setting "waveOutline" as parent
+            pointVec.x = (i + 0.5f) / step - 1f;
             pointVec.y = pointVec.x;
             pointVec.z = 0;
-            pointsList.Add( (GameObject)Instantiate(xPoint, pointVec, Quaternion.identity, wavePositionParent.transform));//adding and instantiating points from the xPoint prefab, setting "waveOutline" as parent
-            }
+            point.transform.localPosition = pointVec;
+            point.transform.localScale = scale;
+            point.transform.SetParent(wavePositionParent.transform, true);
+
+        }
         ScalePointsOutline((pointSpacing + xPoint.transform.localScale.x) * xLength, waveOutline);
     }
 
     public void ScalePointsOutline(float waveWidth, GameObject wave)//functions that scales the waveoutline 
     {
         //parameter waveWidth is the distance needed to be between each point - ie. how long should the plane be?
-        Vector3 temp = wave.transform.localScale;
-        temp.z = waveWidth;
-        temp.y *= 3;
-        wave.transform.localScale = temp;
+        //Vector3 temp = wave.transform.localScale;
+        //temp.z = waveWidth;
+        //temp.y *= 3;
+        //wave.transform.localScale = temp;
+        //wave.transform.localScale = scale;
         print("planeWidth: "+ pointSpacing + " + " + xPoint.transform.localScale.z + " * " + xLength + " = " + waveWidth);
 
     }
@@ -91,7 +102,8 @@ public class GraphControl : MonoBehaviour {
     void Start() 
     {
         wavePrefab = (GameObject)Resources.Load("testWaveOutline", typeof(GameObject));//loading the prefab from the resources folder in order to access its values
-        
+        //step = 2f / xLength;
+        //scale = Vector3.one * step;
     }
 
 
