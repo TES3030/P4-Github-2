aeaa7af7fb2c125a2c7b5dd63bf2982a814bb802
Tobias <tobse.tes@gmail.com>
2018-04-30 13:13:26 +0200

    Split GraphControl script into 2 scripts
    In order to make it possible to have more than 1 graph object

--- Unity Project/Assets/Scripts/GraphControl.cs
+++ Unity Project/Assets/Scripts/GraphControl.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 
 public class GraphControl : MonoBehaviour
 {
-    Presets presets = new Presets(); //Use curve-presets from Presets script.
+    Presets presets;
 
-    [Range(10, 100)] //This makes xLength a slider in the inspector.
-    public int xLength = 10; // Amount of Gameobjects to create to represent a curve in Unity.
+    private int xLength; // Amount of Gameobjects to create to represent a curve in Unity.
     public bool isCurveAnimated = true; //Animation of curve on/off.
     public bool isCurveLined = true; //LineRenderer on/off.
     public bool isLowFreqMode = true; //Turn LowFrequency mode on/off. 
     public FreqModeName freqMode;
     public bool isCurveDotted = false; //Turn gameObjects that create curve on/off.
 
-
     public float lowFreqScaleFactor = 10; //Determine scalefactor between lowfreqmode and highfreqmode.
-
+    
     public GameObject xPoint; //Prefab from which all points are made.
     private GameObject wavePrefab; //Prefab form which all waveoutlines are made.
+    //private GameObject graphHolder; //Prefab form which all grapholders are made.
+    
     public float amplitude = 1;//Amplitude of waveform. 
     public float frequency = 2;//Frequency of waveform. 
 
@@ -28,47 +28,20 @@ public class GraphControl : MonoBehaviour
 
     List<GameObject> pointsList = new List<GameObject>(); //List of all points in a curve. 
 
+    private Color c1 = Color.yellow;
+    private Color c2 = Color.red;
+
     //LineRenderer colors low/high freq
     public Color lowFrequencyColor = Color.green; //Default color for Low Frequecy mode is green.
     public Color highFrequencyColor = Color.blue;//Default color for High Frequency mode is blue.
 
-
-    //Code to randomly change points values - only used for developing.
-    public void randomChange() //Puts in random values across all Gameobjects
-    {
-        Vector3 temp; //temp Vector for temporaily storing the point information!
-        for (int i = 0; i < pointsList.Count; i++) //Loop goes through each object of list and change Y value with random factor
-        {
-            if (pointsList[i] != null) 
-            {
-
-                temp = pointsList[i].transform.position; //into temp temporarily while it is changed. 
-                temp.y *= (Random.Range(-1.0f, 1.0f)); //access and change y value
-                pointsList[i].transform.position = temp;
-
-            }
-            else
-            {
-                Debug.Log("obj is null");
-            }
-        }
-
-        for (int i = 0; i < pointsList.Count; i++) //go through obj's in pointslist
-        {
-            print(pointsList[i]); //Print position of objects to console
-
-        }
-    }
+    
 
     //fucntion that creates points for the lsit and instantiates them
-    public void createAndInstantiatePoints()
+    
+    public void createAndInstantiatePoints(Transform GraphHolderParent)//need to receive xLength and transfrom from this
     {
-
-        GameObject wavePositionParent = new GameObject("WavePosition");//the empty game object containing the position of the curve/wave
-        wavePositionParent.transform.rotation = Quaternion.Euler(0, 90, 0);
-        wavePositionParent.transform.position = new Vector3(0, 0, 0.2f);
-
-        GameObject waveOutline = (GameObject)Instantiate(wavePrefab, wavePositionParent.transform.localPosition, wavePositionParent.transform.localRotation, wavePositionParent.transform) as GameObject;//instantiating the pink outline arround points
+        GameObject waveOutline = (GameObject)Instantiate(wavePrefab, GraphHolderParent.localPosition, GraphHolderParent.localRotation, GraphHolderParent) as GameObject;//instantiating the pink outline arround points
 
         float step = 2f / xLength;
         Vector3 scale = Vector3.one * step;//all cube points are instantiated between -1 and 1
@@ -79,33 +52,19 @@ public class GraphControl : MonoBehaviour
         for (int i = 0; i < xLength; i++)//for-loop instantiating points and adding points to the gameobject points list
         {
             GameObject point;//this is only for reference
-            pointsList.Add(point = (GameObject)Instantiate(xPoint, wavePositionParent.transform.localPosition, Quaternion.Euler(0, 90, 0)) as GameObject);
+            pointsList.Add(point = (GameObject)Instantiate(xPoint, GraphHolderParent.localPosition, Quaternion.Euler(0, 90, 0)) as GameObject);
             //adding and instantiating points from the xPoint prefab at 90 degrees so it advances along the pink outline
 
-            pointVec.x = (i + 0.5f) * step - 1f;//
+            pointVec.x = (i + 0.5f) * step - 1f;
+            pointVec.y = setPointYPosition(pointVec.x);
 
-            switch((int)curvePresetFunction)
-            {
-                case 1:
-                    pointVec.y = presets.squareWave(pointVec.x,frequency,amplitude);
-                    break;
-                case 2:
-                    pointVec.y = presets.sawTooth(pointVec.x, frequency, amplitude);
-                    break;
-                default:
-                    pointVec.y = presets.sine(pointVec.x, frequency, amplitude);
-                    break;
-            }
-            //pointVec.y = pointVec.x * pointVec.x;//change this to change y of cubes
-           
             point.transform.localPosition = pointVec;
             point.transform.localScale = scale;
-            point.transform.SetParent(wavePositionParent.transform, true);
+            point.transform.SetParent(GraphHolderParent.transform, true);
             //setting the parent at the end, and stating "true" in order to let the points keep their values
-
         }   
     }
-
+    
     public void ScalePointsOutline(float waveWidth, GameObject wave)//functions that scales the waveoutline 
     {
         //parameter waveWidth is the distance needed to be between each point - ie. how long should the plane be?
@@ -118,26 +77,53 @@ public class GraphControl : MonoBehaviour
 
     }
 
-
-    void Start()
+    void setXLength(int x)
     {
-        wavePrefab = (GameObject)Resources.Load("testWaveOutline", typeof(GameObject));//loading the prefab from the resources folder in order to access its values                                               
-
-        //LineRenderer creation
+        xLength = x;
         LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
         lineRenderer.widthMultiplier = 0.1f;
         lineRenderer.positionCount = xLength;
 
-        //LineRenderer Color
-        float alpha = 1.0f; //alpha variable used to set parameters of gradient
-         Gradient lowFreqgradient = new Gradient(); //New Gradient so we can change colors
-        lowFreqgradient.SetKeys( //Set Keys of the new gradient, they change between two colors, but for now they are set to the same
-            new GradientColorKey[] { new GradientColorKey(lowFrequencyColor, 0.0f), new GradientColorKey(lowFrequencyColor, 1.0f) },
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
             new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
             );
+        lineRenderer.colorGradient = gradient;
+    }
 
-        lineRenderer.colorGradient = lowFreqgradient; //Set the color to our gradient
+    float setPointYPosition(float x)
+    {
+        float y;
+        switch ((int)curvePresetFunction)
+        {
+            case 1:
+                y = presets.squareWave(x, frequency, amplitude);
+                break;
+            case 2:
+                y = presets.sawTooth(x, frequency, amplitude);
+                break;
+            default:
+                y = presets.sine(x, frequency, amplitude);
+                break;
+        }
+        return y;
+    }
+
+    private void Awake()
+    {
+        presets = new Presets(); //Use curve-presets from Presets script.
+        wavePrefab = (GameObject)Resources.Load("waveOutline", typeof(GameObject));//loading the prefab from the resources folder in order to access its values    
+        setXLength(GameObject.Find("GraphsManager").GetComponent<GraphsManager>().xLengthForNextGraph);
+        
+    }
+
+    void Start()
+    {
+        createAndInstantiatePoints(gameObject.GetComponent<Transform>());
 
     }
 
@@ -185,20 +171,18 @@ public class GraphControl : MonoBehaviour
 
             }
         }
-
         
-          
 
         if (Input.GetKeyDown("backspace"))
         {
             print("Backspace key was pressed");
-            createAndInstantiatePoints();
+            //createAndInstantiatePoints();
         }
 
         if (Input.GetKeyDown("r"))
         {
             print("Space key was pressed");
-            randomChange();
+            //randomChange();
         }
 
 
@@ -258,18 +242,8 @@ public class GraphControl : MonoBehaviour
             {
                 //If we use localPosition in this loop it fucks up... I have no idea why, i'll look into it at some point (tobi)
                 Vector3 position = pointsList[i].transform.position;
-                switch ((int)curvePresetFunction)
-                {
-                    case 1:
-                        position.y = presets.squareWave(position.x + Time.time, frequency, amplitude);
-                        break;
-                    case 2:
-                        position.y = presets.sawTooth(position.x + Time.time, frequency, amplitude);
-                        break;
-                    default:
-                        position.y = presets.sine(position.x + Time.time, frequency, amplitude);
-                        break;
-                }
+                position.y = setPointYPosition(position.x + Time.time);
+
                 pointsList[i].transform.position = position;
             } 
         }
@@ -278,3 +252,31 @@ public class GraphControl : MonoBehaviour
     }
 }
 
+//Code to randomly change points values - only used for developing.
+/*
+public void randomChange() //Puts in random values across all Gameobjects
+{
+    Vector3 temp; //temp Vector for temporaily storing the point information!
+    for (int i = 0; i < pointsList.Count; i++) //Loop goes through each object of list and change Y value with random factor
+    {
+        if (pointsList[i] != null) 
+        {
+
+            temp = pointsList[i].transform.position; //into temp temporarily while it is changed. 
+            temp.y *= (Random.Range(-1.0f, 1.0f)); //access and change y value
+            pointsList[i].transform.position = temp;
+
+        }
+        else
+        {
+            Debug.Log("obj is null");
+        }
+    }
+
+    for (int i = 0; i < pointsList.Count; i++) //go through obj's in pointslist
+    {
+        print(pointsList[i]); //Print position of objects to console
+
+    }
+}
+*/
